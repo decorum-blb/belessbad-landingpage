c867f8a82588405ce27db7fc46c2f3654204f557
Adam Edwards <adamcharlesedwards@gmail.com>
2019-09-07 21:39:43 -0500

    init

---
+++ precache-manifest.154d85e5ec28611057c30a470df0bfe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdec8fd86d742c3178b6a2d5cc439d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "805c2bb9201cffff6721",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "5d0dce747330af4cca54",
+    "url": "/static/js/2.d55cc32f.chunk.js"
+  },
+  {
+    "revision": "805c2bb9201cffff6721",
+    "url": "/static/js/main.7763de39.chunk.js"
+  },
+  {
+    "revision": "63f6d2bc43b34fbbef4c",
+    "url": "/static/js/runtime~main.192d5e64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
